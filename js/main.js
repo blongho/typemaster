e6e8785b3ca5b4c9c193df7d7c3f9c27d306ea3d
Bernard Longho <lobe1602@student.miun.se>
2017-12-26 18:47:19 +0100

    Added necessary styles for the project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-function fillDate()
-{
+/**
+ * Read xml file
+ */
+function loadTexts() {
+	var xhr;
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	} else {
+		// code for older browsers
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xhr.open("GET", "texts.xml",false);
+	xhr.send();
+	var xmlData = xhr.responseText;
+	document.getElementById("testContent").innerHTML = xmlData;
+}
+
+/**
+ * Dynamically set the date value in the footer section
+ */
+function fillDate() {
 	document.getElementById("year").innerHTML =
 		new Date().getFullYear().toString();
 }
 
 window.addEventListener("load", fillDate, false);
+//window.addEventListener("load", loadTexts, false);
